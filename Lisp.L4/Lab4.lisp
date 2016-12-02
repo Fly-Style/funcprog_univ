b1dd9f93359261c096c64955828ed2fa16880e35
Alex <flystyle@MacBook-Pro-Alex-2.local>
2016-12-02 18:15:53 +0200

    add all the labs

--- Lisp.L4/Lab4.lisp
+++ Lisp.L4/Lab4.lisp
@@ -1,6 +1,6 @@
 (defclass word()
   (
-  	 (word-content :accessor word-content)
+  	(word-content :accessor word-content)
     (vowels-part  :accessor vowels-part)
   )
 )
@@ -13,24 +13,10 @@
 	(print (slot-value w 'content))
 )
 
-(defmethod compare_word((w1 word) (w2 word) &key)
-	(equal (slot-value w1 'content) (slot-value w2 'content))
-)
-
 (defun printer (bag-of-words)
 	(loop for word in bag-of-words
 		do
 		(format t "~D <==> ~:S  ~%" (word-content word) (vowels-part word))
-	)
-)
-
-(defun find_word(lst1 lst2)
-	(if (nil lst1)
-		()
-		(if (member (car lst1) lst2 :test #'compare_word)
-			(find_word (cdr lst1) lst2)
-			(car lst1)
-		)
 	)
 )
 
@@ -48,16 +34,13 @@
 )
 
 (defun sorter-by-vowels (bag-of-words)
-	;(sort bag-of-words #'(lambda (p1 p2) (> (vowels-part p1) (vowels-part p2))))
 	(sort bag-of-words #'> :key (lambda (p) (vowels-part p) ))
-	;(printer bag-of-words)
 )
 
 (defun cooker (bag-of-words)
    (loop for word in bag-of-words
    do
    	(vowels-vowels-applier word)
-   	;(format t "~D <==> ~:A ~%" (word-content word) (vowels-part word))
    )
    (sorter-by-vowels bag-of-words)
    (printer bag-of-words)
@@ -65,12 +48,21 @@
 
 (defun vowels-vowels-applier (str)
  	(let ( (vow 0) (!vow 0))
-  		(map nil #'(lambda (c) (if (member c '(#\A #\E #\O #\I #\a #\e #\i #\o)) (incf vow) (incf !vow)) ) (word-content str))
+  		(map nil #'(lambda (c) (if (member c '(#\A #\E #\Y #\O #\I #\U #\a #\e #\y #\i #\o #\u)) (incf vow) (incf !vow)) ) (word-content str))
 	  	(setq len (+ vow !vow))
 	  	(setq particular (/ vow len))
    	(setf (vowels-part str) particular)
    )
 )
 
-(setf bag-of-words (split-to-words "i love this fucking world and i always wanna live on this planet!"))
-(cooker bag-of-words)
+(setq fname "./lab4.txt")
+(let ((in (open fname)))
+(when in
+  (loop for line = (read-line in nil)
+       while line do
+         (setf bag-of-words (split-to-words (read-line in)))
+         (cooker bag-of-words)
+         (print "================")
+  )
+  (close in))
+)
